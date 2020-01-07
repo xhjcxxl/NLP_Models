b08555e5121ff607f8af6e9bc2cb970ba60e8c0f
xhjcxxl <xhjcxxl@163.com>
2020-01-07 19:48:32 +0800

    CNN_classification

---
+++ CNN_Classification/main.py
@@ -0,0 +1,136 @@
+import os
+import argparse
+import datetime
+import torch
+import torchtext.data as data
+import torchtext.datasets as datasets
+import models
+import train_data
+import dataset
+
+
+# 命令行参数解析
+# 输入的参数 都会对参数进行数据处理
+# 如 python main.py -lr 0.01 就可以设置学习率为0.01
+parser = argparse.ArgumentParser(description='CNN 句子分类器')
+
+# 训练相关参数
+parser.add_argument('-lr', type=float, default=0.001, help='初始化学习率[默认: 0.001]')
+parser.add_argument('-epochs', type=int, default=256, help='训练中 总的数据训练轮数[默认: 256]')
+parser.add_argument('-batch-size', type=int, default=64, help='训练中 一个批量的数据个数[默认: 64]')
+parser.add_argument('-log-interval',  type=int, default=1,   help='多少次迭代进行训练打印[default: 1]')
+parser.add_argument('-test-interval', type=int, default=100, help='多少次迭代进行测试[default: 100]')
+parser.add_argument('-save-interval', type=int, default=500, help='多少次迭代进行保存数据[default:500]')
+parser.add_argument('-save-dir', type=str, default='snapshot', help='快照保存位置')
+parser.add_argument('-early-stop', type=int, default=1000, help='效果没有增加 则 多少次迭代停止')
+parser.add_argument('-save-best', type=bool, default=True, help='最好效果时 是否保存数据')
+# data
+parser.add_argument('-shuffle', action='store_true', default=False, help='每轮 是否都进行随机数据')
+# model
+parser.add_argument('-dropout', type=float, default=0.5, help='随机丢弃的概率[默认: 0.5]')
+parser.add_argument('-max-norm', type=float, default=3.0, help='L2正则化[默认: 3.0]')
+parser.add_argument('-embed-dim', type=int, default=128, help='词向量维度[default: 128]')
+parser.add_argument('-kernel-num', type=int, default=100, help='每种卷积核的个数')
+parser.add_argument('-kernel-sizes', type=str, default='3,4,5', help='卷积核的大小 三种')
+parser.add_argument('-static', action='store_true', default=False, help='是否修改词向量')
+# device
+parser.add_argument('-device', type=int, default=-1, help='进行数据迭代的设别 -1表示CPU[默认: -1]')
+parser.add_argument('-no-cuda', action='store_true', default=False, help='不使用GPU')
+# option
+parser.add_argument('-snapshot', type=str, default=None, help='模型快照的文件名[默认: None]')
+parser.add_argument('-predict', type=str, default=None, help='预测给定的句子')
+parser.add_argument('-test', action='store_true', default=False, help='是否测试')
+args = parser.parse_args()
+
+
+# 加载 SST 数据集
+def sst(text_field, label_field, **kwargs):
+    # 采用 torchtext库中的 数据集用于SST
+    train_data, dev_data, test_data = datasets.SST.splits(text_field, label_field, fine_grained=True)
+    # 构建词表，即需要给每个单词编码，也就是用数字表示每个单词，这样才能传入模型
+    text_field.build_vocab(train_data, dev_data, test_data)
+    label_field.build_vocab(train_data, dev_data, test_data)
+    train_iter, dev_iter, test_iter = data.BucketIterator.splits(
+        (train_data, dev_data, test_data),
+        batch_sizes=(args.batch_size, len(dev_data), len(test_data)),
+        **kwargs)
+    return train_iter, dev_iter, test_iter
+
+
+# 加载 MR 数据集
+def mr(text_field, label_field, **kwargs):
+    # 采用自己写的 dataset 数据集用于MR
+    train_data, dev_data = dataset.MR.splits(text_field, label_field)
+    # 构建词表，即需要给每个单词编码，也就是用数字表示每个单词，这样才能传入模型
+    text_field.build_vocab(train_data, dev_data)
+    label_field.build_vocab(train_data, dev_data)
+    # 创建一个迭代器 从数据集中 加载 batches 的数据
+    # 为数据集的多个拆分 创建迭代器 生成 batch
+    train_iter, dev_iter = data.Iterator.splits(
+        (train_data, dev_data),
+        batch_sizes=(args.batch_size, len(dev_data)),
+        **kwargs)
+    return train_iter, dev_iter
+
+
+# 主程序启动的地方
+# 加载数据集
+print("正在加载数据集...")
+# 首先定义字段的处理方法 包含一写文本处理的通用参数的设置
+# lower 是否把数据转化为小写
+# sequential 是否把数据表示成序列，如果是False, 不能使用分词
+text_field = data.Field(lower=True)
+label_field = data.Field(sequential=False)
+# 加载MR的数据集 传入 样本的field和标签的field格式， 设备选择CPU
+train_iter, dev_iter = mr(text_field, label_field, device=-1, repeat=False)
+print(train_iter)
+print(dev_iter)
+print(text_field.vocab)
+print(label_field.vocab)
+
+# 更新参数 并打印
+args.embed_num = len(text_field.vocab)
+args.class_num = len(label_field.vocab)
+args.cuda = (not args.no_cuda) and torch.cuda.is_available(); del args.no_cuda
+print(args.cuda)
+# 将kernel_sizes 划分出来
+args.kernel_sizes = [int(k) for k in args.kernel_sizes.split(',')]
+args.save_dir = os.path.join(args.save_dir, datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S'))
+
+print("\n 参数设置：")
+for attr, value in sorted(args.__dict__.items()):
+    print("\t{}={}".format(attr.upper(), value))
+
+
+# 模型相关设置
+cnn = models.CNNText(args)
+# 存在快照 就加载快照
+if args.snapshot is not None:
+    print("\n加载模型从 {}...".format(args.snapshot))
+    # 加载快照
+    cnn.load_state_dict(torch.load(args.snapshot))
+
+# 如果有 cuda 就调用 cuda 来跑模型
+if args.cuda:
+    torch.cuda.set_device(args.device)
+    cnn = cnn.cuda()
+
+# 训练和测试模型
+# 预测命令存在 则进行预测
+if args.predict is not None:
+    label = train_data.predict(args.predict, cnn, text_field, label_field, False)
+    print('\n[Text]  {}\n[label]  {}\n'.format(args.predict, label))
+# 存在测试命令 就进行测试
+elif args.test:
+    try:
+        train_data.eval(test_iter, cnn, args)
+    except Exception as e:
+        print("\n测试集不存在.\n")
+# 否则就进行训练
+else:
+    print()
+    try:
+        train_data.train(train_iter, dev_iter, cnn, args)
+    except KeyboardInterrupt:
+        print('\n' + '-' * 89)
+        print('退出了')
---
+++ CNN_Classification/models.py
@@ -0,0 +1,67 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+
+class CNNText(nn.Module):
+    """
+    自定义一个 CNN 模型
+    """
+
+    def __init__(self, args):
+        super(CNNText, self).__init__()
+        self.args = args
+        V = args.embed_num  # 词的个数
+        D = args.embed_dim  # 词的维度（一个词的向量）
+        C = args.class_num  # 多少个分类
+        Ci = 1  # 通道数 输入的维度
+        Co = args.kernel_num  # 卷积核的个数（多个卷积）
+        Ks = args.kernel_sizes  # 卷积的尺寸 list[3,4,5]
+
+        # nn.Embedding 词向量化，生成随机矩阵，V行，D列，V*D
+        self.embed = nn.Embedding(V, D)
+        # 创建卷积层 创建一个二维卷积
+        # 这里为什么要用K D呢，因为 在NLP中，只有上下滑动，没有左右滑动
+        # 所以 卷积核的 列数就是固定为 词的维度
+        # Ci 为输入维度 Co 为输出维度
+        # ModuleList是将子Module作为一个List来保存的，可以通过下标进行访问
+        # 定义一层卷积层
+        # 因为 Ks有多个则有多个卷积核， 3*D，4*D，5*D，故 有三个卷积层使用各自的卷积核 分别同时进行运算
+        self.convs1 = nn.ModuleList([nn.Conv2d(Ci, Co, (K, D)) for K in Ks])
+        # 进行dropout处理 避免过拟合
+        # 定义dropout
+        self.dropout = nn.Dropout(args.dropout)
+        # 线性处理 全连接层
+        # 定义全连接层
+        self.fc1 = nn.Linear(len(Ks) * Co, C)
+
+    def conv_and_pool(self, x, conv):
+        """
+        先卷积 然后 使用激活函数 然后使用 最大池化
+        :param x:
+        :param conv:
+        :return:
+        """
+        x = F.relu(conv(x)).squeeze(3)   # 激活函数 并压缩维度 在第三维度上压缩一个维度
+        x = F.max_pool1d(x, x.size(2)).squeeze(2)   # 最大池化 选择最大那个 在第二维度上压缩一个维度
+        return x
+
+    def forward(self, x):
+        # 前向传播 通过模型计算预测值
+        x = self.embed(x)  # (N, W, D)
+        if self.args.static:
+            x = Variable(x)
+
+        x = x.unsqueeze(1)  # 先将数据 在第一维度上扩张一个维度 (N, Ci, W, D)
+        # 卷积层 + 激活函数
+        x = [F.relu(conv(x)).squeeze(3) for conv in self.convs1]
+        # 池化层
+        x = [F.max_pool1d(i, i.size(2)).squeeze(2) for i in x]
+        # 按列拼接数据
+        x = torch.cat(x, 1)
+        # 随机丢弃处理
+        x = self.dropout(x)
+        # 全连接层
+        logit = self.fc1(x)
+        return logit
